0c9dd955ff61b10a37cc37423decc528f18fa579
Obieze Ezeugo <felistusobieze@gmail.com>
2019-10-30 14:43:43 +0100

    added a new functionality(using the enterKey to submit) and also redesigned the UI

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
 
+// Using the Submit button to submit the input text
     $("#btn").click(() => {
        let inputText = $("#taskField").val();
         if (inputText === "") {
@@ -15,6 +16,24 @@ $(document).ready(() => {
                                    $("#taskField").val("");
         }
     }); 
+// Using the KeyBoard ENTER key to submit the input text
+    $("#taskField").keypress( (event) => {
+        if (event.keyCode === 13) {
+            let inputText = $("#taskField").val();
+            if (inputText === "") {
+    
+            } else {
+                $("#unorderedList").append( `<div class="item"> 
+                                             <li>
+                                                <input type="checkbox" name="check" class="checkbox"> 
+                                                <label for="check"> ${ inputText } </label>
+                                            </li> 
+                                            <i class="fa fa-times delete"></i>
+                                        </div> `);
+                                       $("#taskField").val("");
+            }
+        }
+    });
 
     $("#unorderedList").on("click", ".checkbox", function() {
         $(this).parent().toggleClass("strike");
@@ -24,9 +43,7 @@ $(document).ready(() => {
         $(this).parent().remove();
     });
 
-    $(".circle-1").click( ()=> {
-       $(".item").remove();
-    });
+
     
 });
 
